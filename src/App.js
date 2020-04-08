8ada1a1359b45d4d6417bbcb8a09c4f88283587f
Prathameshmule2415 <59178753+Prathameshmule2415@users.noreply.github.com>
2020-04-08 21:56:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NavBar from "./navbar.component";
+import Jumbo from "./jumbotron.component";
+import Feature from "./feature.component";
+import GetTouch from "./getintouch.component";
+import Footer from "./footer.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Jumbo />
+      <Feature />
+      <GetTouch />
+      <Footer />
     </div>
   );
 }
